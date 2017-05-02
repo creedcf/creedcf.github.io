8652961be5da849f19c07d1b5b38277cf24233c5
creedcf <admin@creed.cf>
2017-05-02 16:48:32 +0900

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+(function($){
+  $(function(){
+
+    $('.button-collapse-2').sideNav2();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
